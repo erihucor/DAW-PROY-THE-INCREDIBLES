a80fadaf8288d444ec6cce293a8c033af95e5462
ericksitocg <ericksitocg@hotmail.com>
2019-07-12 15:34:22 -0500

    por fin

--- js/buscarProducto.js
+++ js/buscarProducto.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
             ///////////////// Logica de funcion clic
             $(".boton-buscador").click(function(){
                     var nombre = $('.buscador-input').val();
+					$('.combo_productos').empty();
+					
 					if(nombre.toLowerCase() == 'agua'){
-                        let elemento = $('<li> agua </li>')
+                        let elemento = $('<li class = "text-center"> <a href="agua.html" >agua</a> </li>')
                         $(".combo_productos").append(elemento);
                     }
                     if ((nombre.toLowerCase() == 'bebida') || (nombre.toLowerCase() == 'bebidas') ){
-                        let elementoB = $('<li> bebida energetica </li>')
+                        let elementoB = $('<li class = "text-center"> <a href="energizantes.html" >bebida energetica </a> </li>')
                         $(".combo_productos").append(elementoB);
-                        let elementoB2 = $('<li> bebida saborizada </li>')
+                        let elementoB2 = $('<li class = "text-center"> <a href="bsaborizadas.html" > bebida saborizada </li>')
                         $(".combo_productos").append(elementoB2);
 
                     }
-
             })
 })
